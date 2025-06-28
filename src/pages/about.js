613d998d3211d559bd722f8fd59e74fe9f4f738c
Baldwin Cepeda <baldwin@Baldwins-MacBook-Pro.local>
2025-06-28 18:37:19 -0400

    fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,9 +8,9 @@ export default function About() {
             <main className="max-w-3xl mx-auto px-6 py-16 text-gray-800">
                 <h2 className="text-4xl font-bold mb-4">About Me</h2>
                 <p className="text-lg leading-relaxed">
-                    Hi, I'm [Your Name], a passionate photographer based in [Your Location].
+                    Hi, I am Jose Antonio Carrion Moscoso , a passionate photographer based in NYC.
                     I specialize in capturing authentic moments through natural light and storytelling.
-                    Whether it's a wedding, portrait session, or a lifestyle shoot, I aim to create timeless memories.
+                    Whether it is a wedding, portrait session, or a lifestyle shoot, I aim to create timeless memories.
                 </p>
             </main>
             <Footer />
